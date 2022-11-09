25ab2ebe14d63a21976a7f0d53566c4145312106
rinriN <2210368@ecc.ac.jp>
2022-11-09 23:52:34 +0900

    :memo: : 初始化函數

--- go35/main.go
+++ go35/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func init() {
+	fmt.Println("初始化函數 init1('系統初始化')")
+}
+func init() {
+	fmt.Println("初始化函數 init2('環境初始化')")
+}
+func init() {
+	fmt.Println("初始化函數 init3('資料庫初始化')")
+}
+
+func main() {
+	fmt.Println("主線程運行開始...")
 }
