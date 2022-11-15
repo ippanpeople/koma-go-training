45e56dfba9377420db5c310d50bd8d32540ecbaa
rinriN <2210368@ecc.ac.jp>
2022-11-15 20:28:44 +0900

    :memo: 利用vscode生成測試代碼

---
+++ go39/utils/math_test.go
@@ -0,0 +1,29 @@
+package utils
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	type args struct {
+		x int
+		y int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "正常處理",
+			args: args{x: 10, y: 20},
+			want: 30,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Add(tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("Add() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
